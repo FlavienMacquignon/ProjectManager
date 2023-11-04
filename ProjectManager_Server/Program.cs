85bd63ff96e32c925e01f81240fd66a6cc3a4267
Flavien.Macquignon <flavien_macquignon@fastmail.fr>
2023-11-04 13:18:20 +0100

    MVP with Swagger

--- ProjectManager_Server/Program.cs
+++ ProjectManager_Server/Program.cs
@@ -1,6 +1,10 @@
+using System;
+using System.IO;
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace ProjectManager_Server;
 
@@ -10,29 +14,61 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
+        AssemblyName currentAssem = Assembly.GetExecutingAssembly().GetName();
+        var name= currentAssem.Name;
+        var version = currentAssem.Version;
+        Console.Error.WriteLine("Name :: "+name +" Version :: "+version);
         // Add services to the container.
 
         builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+        builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc($"{version}", new OpenApiInfo
+                {
+                    Version = $"{version}",
+                    Title = "ProjectManager API",
+                    Description = "A Jira clone in AspNet",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Flavien Macquignon",
+                        Email = "[email protected]",
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under GPL-3.0",
+                        Url = new Uri("https://www.gnu.org/licenses/gpl-3.0.en.html#license-text"),
+                    }
+                }
+                );
+                c.EnableAnnotations();
+                var xmlFilename = $"{name}.xml";
+                c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+            });
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
+        app.UseDeveloperExceptionPage();
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
         {
-            app.UseSwagger();
-            app.UseSwaggerUI();
+            c.SwaggerEndpoint($"/swagger/{version}/swagger.json", "My API V1");
+            c.RoutePrefix = "swagger";            
         }
+        );
 
-        app.UseHttpsRedirection();
+        app.UseRouting();
+        // app.UseHttpsRedirection();
 
-        app.UseAuthorization();
-
-
-        app.MapControllers();
-
+        // app.UseAuthorization();
+        app.UseStaticFiles();
+        app.UseDeveloperExceptionPage();
+        app.UseEndpoints(endpoints =>
+                 {
+                     endpoints.MapControllers();
+                 });
         app.Run();
     }
 }
